c6082b3f6d6a85891d40f5032f3b8e49ac563bc9
Sinan <sinnandogann@outlook.com>
2023-03-30 14:55:22 +0300

    Add project files.

---
+++ Distributed.Caching/Controllers/ExampleController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text;
+
+namespace Distributed.Caching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExampleController : ControllerBase
+    {
+        readonly IDistributedCache _distributedCache;
+
+        public ExampleController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        [HttpGet("set")]
+        public async Task<IActionResult> Set(string name, string surname)
+        {
+            try
+            {
+                await _distributedCache.SetStringAsync("name", name, options: new()
+                {
+                    AbsoluteExpiration=DateTime.Now.AddSeconds(30),
+                    SlidingExpiration=TimeSpan.FromSeconds(5)
+                });
+                await _distributedCache.SetAsync("surname", Encoding.UTF8.GetBytes(surname), options: new()
+                {
+                    AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                    SlidingExpiration = TimeSpan.FromSeconds(5)
+                });
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, ex.Message);
+            }
+
+        }
+
+
+        [HttpGet("get")]
+
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var name = await _distributedCache.GetStringAsync("name");
+                var surnameBinary = await _distributedCache.GetAsync("surname");
+                var surname = Encoding.UTF8.GetString(surnameBinary);
+                return Ok(new
+                {
+                    name,
+                    surname
+                });
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, ex.Message);
+            }
+
+        }
+
+    }
+}
---
+++ InMemory.Caching/Controllers/ValuesController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemory.Caching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        readonly IMemoryCache _memoryCache;
+
+        public ValuesController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+
+
+        #region Basic Set and Get example
+        [HttpGet]
+        public string Get()
+        {
+            _memoryCache.Set("name", "xyzt");
+            return _memoryCache.Get<string>("name");
+        }
+        #endregion
+
+
+        #region TryGetValue example
+        [HttpGet("set/{name}")]
+        public void setName(string name)
+        {
+            _memoryCache.Set("name", name);
+        }
+
+
+        [HttpGet]
+
+        public string GetName()
+        {
+
+            var check = _memoryCache.TryGetValue<string>("name", out string name);
+
+            if (check)
+            {
+                return name;
+            }
+
+            return "";
+
+
+        }
+        #endregion
+
+
+
+        #region Absolute and Sliding Time Example
+        [HttpGet("setDate")]
+        public void SetDate()
+        {
+            _memoryCache.Set<DateTime>("date", DateTime.Now, options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(5)
+            });
+        }
+
+
+        [HttpGet("getDate")]
+        public DateTime GetDate()
+        {
+            return _memoryCache.Get<DateTime>("date");
+        }
+        #endregion
+
+
+    }
+}
