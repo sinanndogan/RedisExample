9b3d68db1377a74f97965c773a96ef33f68f6655
Sinan <sinnandogann@outlook.com>
2023-03-30 16:29:43 +0300

    eklemeler yapıldı

---
+++ Redis.Pub.Sub.Example/Class1.cs
@@ -0,0 +1,14 @@
+﻿using StackExchange.Redis;
+
+ConnectionMultiplexer redis= await ConnectionMultiplexer.ConnectAsync("localhost:1453");
+
+
+ISubscriber subscriber=redis.GetSubscriber();
+
+//bir publisher ürettik mesaj girilen mesajı subs'lara dağıtacak
+while (true)
+{
+    Console.Write("Message : ");
+    string message=Console.ReadLine();
+    await subscriber.PublishAsync("mychannel", message);
+}
