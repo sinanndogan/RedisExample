50f544f123936869d47e716ba70ad2c1b13e9bed
Sinan Dogan <sinnandogann@outlook.com>
2023-03-31 11:17:15 +0300

    Pub/Sub örneği yapıldı

---
+++ Redis.Pub.Sub.Example1/Program.cs
@@ -0,0 +1,15 @@
+﻿using StackExchange.Redis;
+
+ConnectionMultiplexer redis = await ConnectionMultiplexer.ConnectAsync("localhost:yourlocalhost");
+
+
+
+ISubscriber subscriber=redis.GetSubscriber();
+
+
+while (true)
+{
+    Console.Write("Mesaj : ");
+    string message =Console.ReadLine();
+    subscriber.PublishAsync("mychannel",message);
+}
---
+++ Redis.Pub.Sub.Subscriber.Example1/Program.cs
@@ -0,0 +1,12 @@
+﻿using StackExchange.Redis;
+
+ConnectionMultiplexer redis = await ConnectionMultiplexer.ConnectAsync("localhost:yourlocalhost");
+
+ISubscriber subscriber = redis.GetSubscriber();
+
+ await subscriber.SubscribeAsync("mychannel", (channel, message) =>
+{
+    Console.WriteLine(message);
+});
+
+Console.Read();
